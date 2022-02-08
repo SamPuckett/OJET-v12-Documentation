f743f600c15afe8d965ab615faa0ae16709c8ed0
Samuel Ruiz <samuelpuckett77@gmail.com>
2022-02-07 17:02:05 -0800

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,55 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { useState } from 'react';
+import { AppShell, Burger, Header, MediaQuery, Navbar, Text, useMantineTheme } from '@mantine/core';
 
 export default function HomePage() {
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
   return (
-    <>
-      <Title sx={{ fontSize: 100, fontWeight: 900, letterSpacing: -2 }} align="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span">
-          Mantine
-        </Text>
-      </Title>
-      <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js projects includes a minimal setup for server side rendering, if you
-        want to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/theming/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
-      </Text>
-    </>
+    <AppShell
+      // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+      navbarOffsetBreakpoint="sm"
+      // fixed prop on AppShell will be automatically added to Header and Navbar
+      fixed
+      navbar={
+        <Navbar
+          padding="md"
+          // Breakpoint at which navbar will be hidden if hidden prop is true
+          hiddenBreakpoint="sm"
+          // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
+          hidden={!opened}
+          // when viewport size is less than theme.breakpoints.sm navbar width is 100%
+          // viewport size > theme.breakpoints.sm – width is 300px
+          // viewport size > theme.breakpoints.lg – width is 400px
+          width={{ sm: 300, lg: 400 }}
+        >
+          {/* First section with normal height (depends on section content) */}
+          <Navbar.Section>First section</Navbar.Section>
+          {/* Grow section will take all available space that is not taken by first and last sections */}
+          <Navbar.Section grow mt="lg">Grow section</Navbar.Section>
+          {/* Last section with normal height (depends on section content) */}
+          <Navbar.Section>Last section</Navbar.Section>
+        </Navbar>
+      }
+      header={
+        <Header height={70} padding="md">
+          {/* Handle other responsive styles with MediaQuery component or createStyles function */}
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>JET v12 Documentation</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Text>Resize app to see responsive navbar in action</Text>
+    </AppShell>
   );
 }
